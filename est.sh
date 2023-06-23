32f35fa2f036233963061d189e4adae6936cebc5
NodersUA Team <nodersua@gmail.com>
2023-06-23 19:48:03 +0300

    Update est.sh

--- est.sh
+++ est.sh
@@ -2,9 +2,9 @@
 
 count=280
 timeout=$((3600 / count / 2))
-if [ $timeout -lt 14 ]; then timeout=14; fi
+if [ $timeout -lt 12 ]; then timeout=12; fi
 
-sleep $(shuf -i 60-300 -n 1)
+sleep $(shuf -i 60-180 -n 1)
 
 execute_with_sequence_check() {
   cmd=$1
@@ -25,16 +25,16 @@ denom=$(shuf -n 1 credits.txt)
 echo ">>> Credit ${i}: ${denom}"
 
 # buy credit
-execute_with_sequence_check "empowerd tx wasm execute empower14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sfg4umu '{\"buy_credits\":{\"owner\":\"empower10meawsx9u3lkl2p6amk87k6tye6qn6dkqx2ksj\",\"denom\":\"${denom}\",\"number_of_credits_to_buy\":1}}' --amount 500000umpwr --from wallet --gas 500000 --fees $(shuf -i 100-250 -n 1)00umpwr"
+execute_with_sequence_check "empowerd tx wasm execute empower14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sfg4umu '{\"buy_credits\":{\"owner\":\"empower10meawsx9u3lkl2p6amk87k6tye6qn6dkqx2ksj\",\"denom\":\"${denom}\",\"number_of_credits_to_buy\":1}}' --amount 500000umpwr --from wallet --gas 500000 --fees $(shuf -i 12500-20000 -n 1)umpwr"
 
 if [ $(shuf -i 1-2 -n 1) == 1 ]; then
 echo "tranfer credit"
 # tranfer credit
-execute_with_sequence_check "empowerd tx plasticcredit transfer ${wallet} empower175p8jy5fcdkpm3djk40p8ucdn3lyjyd7jtf77w ${denom} 1 false --gas 500000 --fees $(shuf -i 100-250 -n 1)00umpwr"
+execute_with_sequence_check "empowerd tx plasticcredit transfer ${wallet} empower175p8jy5fcdkpm3djk40p8ucdn3lyjyd7jtf77w ${denom} 1 false --gas 500000 --fees $(shuf -i 12500-20000 -n 1)umpwr"
 else
 echo "retire credit"
 # retire
-execute_with_sequence_check "empowerd tx plasticcredit retire $denom 1 $MONIKER $MONIKER --from wallet --gas 500000 --fees $(shuf -i 100-250 -n 1)00umpwr"
+execute_with_sequence_check "empowerd tx plasticcredit retire $denom 1 $MONIKER $MONIKER --from wallet --gas 500000 --fees $(shuf -i 12500-20000 -n 1)umpwr"
 fi
 
 done
